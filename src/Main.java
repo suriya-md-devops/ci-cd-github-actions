4ad8f89543a65b12bd433a73370dab1908651f59
Suriya M <suriyamurugesan2k5@gmail.com>
2026-06-05 12:42:52 +0000

    Add GitHub Actions CI/CD project

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("CI/CD Pipeline using GitHub Actions");
+        System.out.println("Build completed successfully");
+    }
+}
